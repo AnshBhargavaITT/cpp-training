4f6776889413e5f6a35ed421d1c5cd20e2ad8249
Ansh Bhargava <ansh.bhargava@intimetec.com>
2025-07-09 12:03:23 +0530

    Practice enum

--- week1/08-07-2025/enum.cpp
+++ week1/08-07-2025/enum.cpp
@@ -15,5 +15,11 @@ int main()
     Week week=FRIDAY;
     //Week week=SUNDAY;
     cout<<week<<endl;
+    week=static_cast<Week>(WEDNESDAY+FRIDAY);
+    cout<<week<<endl;
+    for(Week w=MONDAY;w<=SATURDAY;w=static_cast<Week>(w+1))
+    {
+        cout<<w<<endl;
+    }
     return 0;
 }
