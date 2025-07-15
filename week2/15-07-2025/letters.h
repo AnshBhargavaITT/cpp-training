025830ab40c6efdeebfff873c1b27846c072e815
Ansh Bhargava <ansh.bhargava@intimetec.com>
2025-07-15 17:08:28 +0530

    Practice Code

---
+++ week2/15-07-2025/letters.h
@@ -0,0 +1,9 @@
+#ifndef LETTER_H
+#define LETTER_H
+
+int getNumber()
+{
+    return 26;
+}
+
+#endif
---
+++ week2/15-07-2025/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "letters.h"
+
+int main()
+{
+    std::cout<<getNumber()<<std::endl;
+}
