b91ed8a7538679363586a6893159e57351e0be44
Ansh Bhargava <ansh.bhargava@intimetec.com>
2025-08-18 22:37:40 +0530

    Practice Code - 18-08-2025

---
+++ week7/18-08-2025/factorial_test/factorial.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <gtest/gtest.h>
+
+int Factorial(int n)
+{
+    if(n<=1)
+    return 1;
+    return n*Factorial(n-1);
+}
+
+TEST(FactorialTest, Zero) 
+{
+    EXPECT_EQ(Factorial(0), 1);
+}
+
+TEST(FactorialTest, Positive) 
+{
+    EXPECT_EQ(Factorial(1), 1);
+    EXPECT_EQ(Factorial(2), 2);
+    EXPECT_EQ(Factorial(3), 6);
+    EXPECT_EQ(Factorial(4), 24);
+    EXPECT_EQ(Factorial(5), 120);
+}
+
+TEST(FactorialTest, Negative)
+{
+    EXPECT_EQ(Factorial(-1),1);
+    EXPECT_EQ(Factorial(-5),1);
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ week7/18-08-2025/prime_number_test/build/CMakeFiles/MyTests.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anshbhargava/Desktop/cpp-training/week7/18-08-2025/prime_number_test/primeNumber.cpp" "CMakeFiles/MyTests.dir/primeNumber.cpp.o" "gcc" "CMakeFiles/MyTests.dir/primeNumber.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
